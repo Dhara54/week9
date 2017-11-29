caf85c7b3383bc753ab0d27294b982d4f109c5fe
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-11-29 00:26:40 -0500

    database connection

---
+++ classes/database/dbconn.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+define('DATABASE', 'dbp54');
+define('USERNAME', 'dbp54');
+define('PASSWORD', 'ewKtFdfg');
+define('CONNECTION', 'sql1.njit.edu');
+
+class dbConn{
+
+    //variable to hold connection object.
+    protected static $db;
+
+    //private construct - class cannot be instatiated externally.
+    private function __construct() {
+
+        try {
+            // assign PDO object to db variable
+            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            echo "connection done";
+            echo "<br>";
+        }
+        catch (PDOException $e) {
+            //Output error - would normally log this to error file rather than output to user.
+            echo "Connection Error: " . $e->getMessage();
+        }
+
+    }
+
+    // get connection function. Static method - accessible without instantiation
+    public static function getConnection() {
+
+        //Guarantees single instance, if no connection object exists then create one.
+        if (!self::$db) {
+            //new connection object.
+            new dbConn();
+        }
+
+        //return connection.
+        return self::$db;
+    }
+}
+
+
+
+?>
